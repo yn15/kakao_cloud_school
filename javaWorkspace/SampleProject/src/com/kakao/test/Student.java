2ed2f3fb50fdc243c1f61564c37f5cfba47a2800
yn15 <68692931+yn15@users.noreply.github.com>
2022-07-11 17:14:19 +0900

    Java 접근자

---
+++ javaWorkspace/SampleProject/src/com/kakao/test/Student.java
@@ -0,0 +1,11 @@
+package com.kakao.test;
+
+public class Student {
+	
+	String stuName;
+	public String stuNum;
+	
+	public Student() {
+		
+	}
+}
---
+++ javaWorkspace/SampleProject/src/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+import com.kakao.test.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.stuNum = "010";
+	}
+	
+}
