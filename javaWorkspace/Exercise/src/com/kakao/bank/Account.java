11f01092a13899b755ee85007081e0a7c20a1432
yn15 <68692931+yn15@users.noreply.github.com>
2022-07-12 17:16:57 +0900

    bank exercise

--- javaWorkspace/Exercise/src/com/kakao/bank/Account.java
+++ javaWorkspace/Exercise/src/com/kakao/bank/Account.java
@@ -1,9 +1,9 @@
 package com.kakao.bank;
 
 public class Account {
-	private String name;
-	private String account;
-	private int balance;
+	private String name; // 예금주
+	private String account; // 계좌
+	private int balance; // 잔액
 	
 	public Account() {
 		
@@ -18,26 +18,29 @@ public class Account {
 		accountBalance();
 	}
 	
+	// 계좌정보 출력
 	public void printAccount(){
 		System.out.println("계좌 " + getAccount() + "(예금주 : " + getName() + ")");
 	}
 	
+	// 입금
 	public void deposit(int money) {
 		System.out.println(money + " 원 입금합니다.");
 		this.balance += money;
 		accountBalance();
 	}
 	
+	// 출금
 	public void withdraw(int money) {
 		System.out.println(money + " 원 출금합니다.");
 		this.balance -= money;
 		accountBalance();
 	}
 	
+	// 잔액
 	public void accountBalance() {
 		System.out.println("잔액 : " + getBalance() + "원");
 	}
-	
 	
 	
 	public String getName() {
@@ -68,7 +71,6 @@ public class Account {
 		Account b1 = new Account("홍길동", "123-45678", 10000);
 		b1.deposit(20000);
 		b1.withdraw(45000);
-		
 	}
 
 }
