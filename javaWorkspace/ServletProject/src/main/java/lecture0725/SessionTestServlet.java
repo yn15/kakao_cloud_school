1a39a3a3af7186ed2e805cbb0a222da13203263c
yn15 <68692931+yn15@users.noreply.github.com>
2022-07-25 19:09:46 +0900

    Session 실습

---
+++ javaWorkspace/ServletProject/src/main/java/lecture0725/SessionTestServlet.java
@@ -0,0 +1,50 @@
+package lecture0725;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionTestServlet
+ */
+@WebServlet("/mysession")
+public class SessionTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionTestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 입력
+		String id = request.getParameter("myID");
+		
+		// 2. 로직
+		// 해당 client에게 session을 할다아하거나 할당된 session을 찾아와요
+		HttpSession session = request.getSession();
+		session.setAttribute("id", id);
+		
+		
+		// 3. 출력
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
