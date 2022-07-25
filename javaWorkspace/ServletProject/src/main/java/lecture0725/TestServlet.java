18b6735df3315b8ec7e48fde7641114a438c84a9
yn15 <68692931+yn15@users.noreply.github.com>
2022-07-25 19:08:42 +0900

    Servlet 기본 실습

---
+++ javaWorkspace/ServletProject/src/main/java/lecture0725/TestServlet.java
@@ -0,0 +1,49 @@
+package lecture0725;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/hello")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 입력받음
+		// 2. 로직처리
+		// 3. 클라이언트에게 결과 출력
+		// 클라이언트에게 특정 문자열을 출력해야 되기 때문에 OuputStream이 필요
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = new PrintWriter(response.getOutputStream());
+		out.println("<html><head><body><h1>하이하이</h1></body></head></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
