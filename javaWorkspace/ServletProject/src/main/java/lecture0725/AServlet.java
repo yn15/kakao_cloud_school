9a898c911ed9686823cfa854776ef8050be534a0
yn15 <68692931+yn15@users.noreply.github.com>
2022-07-25 19:09:28 +0900

    ServletContext 실습

---
+++ javaWorkspace/ServletProject/src/main/java/lecture0725/AServlet.java
@@ -0,0 +1,50 @@
+package lecture0725;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AServlet
+ */
+@WebServlet("/servleta")
+public class AServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 입력
+		// 2. 로직
+		// ServletContext 사용
+		// ServletContext 객체를 얻어와요(공유객체에 대한 Reference를 획득)
+		ServletContext context = getServletContext();
+		
+		// ServletContext에 특정 정보를 저장해보아요
+		context.setAttribute("product",  "냉장고");
+		// 3. 출력
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ javaWorkspace/ServletProject/src/main/java/lecture0725/BServlet.java
@@ -0,0 +1,51 @@
+package lecture0725;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BServlet
+ */
+@WebServlet("/servletb")
+public class BServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 입력
+		// 2. 로직
+		ServletContext context = getServletContext();
+		String product = (String)context.getAttribute("product");
+		// 3.출력
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = new PrintWriter(response.getOutputStream());
+		out.println("얻어온 결과는 : " + product);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
