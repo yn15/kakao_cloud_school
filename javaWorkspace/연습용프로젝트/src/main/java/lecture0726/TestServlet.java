740b691f0f421c540f6be9762d2277fc6449badc
yn15 <68692931+yn15@users.noreply.github.com>
2022-07-26 20:24:03 +0900

    Servlet 실습

---
+++ javaWorkspace/연습용프로젝트/src/main/java/lecture0726/TestServlet.java
@@ -0,0 +1,48 @@
+package lecture0726;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 입력
+		// 2. 로직
+		// 3. 출력
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = new PrintWriter(response.getOutputStream());
+		out.println("소리없는 아우성!");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
