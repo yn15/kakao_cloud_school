9c6fa1ffdbb083e8be755c551f3bbc2cb537c007
yn15 <68692931+yn15@users.noreply.github.com>
2022-08-01 19:45:38 +0900

    user vo

---
+++ javaWorkspace/Board/src/main/java/user/vo/User.java
@@ -0,0 +1,28 @@
+package user.vo;
+
+public class User {
+	private String userID;
+	private String userPW;
+	private String userName;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPW() {
+		return userPW;
+	}
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
