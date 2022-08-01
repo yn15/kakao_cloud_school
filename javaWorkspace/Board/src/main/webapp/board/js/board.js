7a87f176526a13362d396f349919b0418b662735
yn15 <68692931+yn15@users.noreply.github.com>
2022-08-01 19:50:24 +0900

    board js

---
+++ javaWorkspace/Board/src/main/webapp/board/js/board.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+ 
+ function getBoard(bNumber, bHits){
+	//let requestURL = "http://localhost:8080/board/boarddetail" + "?bNumber=" + bNumber;
+	//window.location.href = requestURL;
+	var form = document.createElement('form'); // 폼객체 생성
+	form.setAttribute('method', 'post'); //get,post 가능
+	form.setAttribute('action', "/board/boarddetail"); //보내는 url
+	var objs;
+	objs = document.createElement('input'); // 값이 들어있는 녀석의 형식
+	objs.setAttribute('type', 'hidden'); // 값이 들어있는 녀석의 type
+	objs.setAttribute('name', 'bNumber');
+	objs.setAttribute('value', bNumber); // 값이 들어있는 녀석의 type
+	form.appendChild(objs);
+	
+	var objs2;
+	objs2 = document.createElement('input');
+	objs2.setAttribute('type', 'hidden'); // 값이 들어있는 녀석의 type
+	objs2.setAttribute('name', 'bHits');
+	objs2.setAttribute('value', bHits);
+	form.appendChild(objs2);
+	
+	document.body.appendChild(form);
+	form.submit();
+}
+
