fa3898d9e50a2e7963c34c15e6fcd104034a7679
yn15 <68692931+yn15@users.noreply.github.com>
2022-08-01 19:47:19 +0900

    게시글 삭제 Controller

---
+++ javaWorkspace/Board/src/main/java/board/controller/BoardDeleteController.java
@@ -0,0 +1,54 @@
+package board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.service.BoardService;
+
+/**
+ * Servlet implementation class BoardDeleteController
+ */
+@WebServlet("/boarddelete")
+public class BoardDeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardDeleteController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String bNumber = request.getParameter("bNumber");
+		
+		BoardService service = new BoardService();
+		int result = service.boarddelete(bNumber);
+		
+		if(result != 0 ) {
+			RequestDispatcher rd = request.getRequestDispatcher("/boardlist");
+			rd.forward(request, response);
+		}else {
+			response.sendRedirect("/board/AccessFail.html");
+		}
+	}
+
+}
