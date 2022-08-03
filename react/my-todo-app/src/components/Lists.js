2778c7f4a9df98261bee896c1442c64a4983d494
yn15 <68692931+yn15@users.noreply.github.com>
2022-08-03 16:04:50 +0900

    Lists Drag and Drop 보이는 형식만 구현

--- react/my-todo-app/src/components/Lists.js
+++ react/my-todo-app/src/components/Lists.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 // 구조분해할당
 export default function Lists({todoData, setTodoData}) {
@@ -21,21 +22,40 @@ export default function Lists({todoData, setTodoData}) {
 
     return (
         <div>
-            {todoData.map(data => (
-                <div key={data.id}>
-                    <div className='flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded'>
-                        <div className='items-center'>
-                            <input type="checkbox"
-                                   defaultChecked={false}
-                                   onChange={() => handleCompleteChange(data.id)} />
-                            {" "}<span className={data.completed ? "line-through" : undefined}>{data.title}</span>
+            <DragDropContext>
+                <Droppable droppableId='to-do'>
+                    {(provided) => (
+                        <div {...provided.droppableProps} ref={provided.innerRef}>
+                            {todoData.map((data, index) => (
+                                <Draggable 
+                                    key={data.id} 
+                                    draggableId={data.id.toString()} 
+                                    index={index}>
+                                    {(provided, snapshot) => (
+                                        <div key={data.id} 
+                                            {...provided.draggableProps} 
+                                            ref={provided.innerRef}
+                                            {...provided.dragHandleProps}>
+                                            <div className={`${snapshot.isDragging? " bg-gray-500 " : " bg-gray-100 "}flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}>
+                                                <div className='items-center'>
+                                                    <input type="checkbox"
+                                                        defaultChecked={false}
+                                                        onChange={() => handleCompleteChange(data.id)} />
+                                                    {" "}<span className={data.completed ? "line-through" : undefined}>{data.title}</span>
+                                                </div>
+                                                <div className='items-center'>
+                                                    <button onClick={() => deleteClick(data.id)}>delete</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
                         </div>
-                        <div className='items-center'>
-                            <button onClick={() => deleteClick(data.id)}>delete</button>
-                        </div>
-                    </div>
-                </div>
-            ))}
+                    )}
+                </Droppable>
+            </DragDropContext>
         </div>
     )
 }
--- react/my-todo-app/src/index.js
+++ react/my-todo-app/src/index.js
@@ -6,9 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App /> // react-beautiful-dnd 를 사용하기 위해선 react 16, 17버젼이 필요한데 현재 18 버젼이기때문에 해제
+  // </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
