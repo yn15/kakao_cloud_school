5fbe1a44339a1e0c8a6f4c9d07493ff69b1566a7
yn15 <68692931+yn15@users.noreply.github.com>
2022-08-23 16:09:05 +0900

    ComponentScan, Autowired ADD

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type =  FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
