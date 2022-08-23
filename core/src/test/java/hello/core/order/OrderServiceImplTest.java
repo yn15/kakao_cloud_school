5ab0a47a879a2d884202ce7c8fc32b54e1ec4e79
yn15 <68692931+yn15@users.noreply.github.com>
2022-08-23 19:19:04 +0900

    Autowired Option (require, Nullable, Optional) Test

---
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
