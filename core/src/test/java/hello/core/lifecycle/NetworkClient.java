cca2d29bc36d8d54ef0321a21579984cd7a5a5f9
yn15 <68692931+yn15@users.noreply.github.com>
2022-08-24 16:19:48 +0900

    Bean LifeCycle - @PostConstruct, @PreDestroy Test

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient  {
 
     private String url;
@@ -23,6 +26,7 @@ public class NetworkClient  {
     }
 
     // 서비스 종료시 호출
+    @PostConstruct
     public void disConnect() {
         System.out.println("close: " + url);
     }
@@ -32,7 +36,7 @@ public class NetworkClient  {
         connect();
         call("초기화 연결 메시지");
     }
-
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disConnect();
